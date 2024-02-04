a5c684761adbbe441420b27057c91d3bff0be76d
shox701 <ebrbrn71@gmail.com>
2024-02-05 00:02:03 +0500

    v 2.0

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 let elMovList = document.querySelector('.movies__list')
 let partMovies = movies.slice(0, 100)
 fnRender(partMovies)
-function fnRender(data) {
+function fnRender(data){
+  let arrLocDataHeart = JSON.parse(window.localStorage.getItem('locdata'))
   elMovList.innerHTML = ''
   data.forEach((item, index) => {
     let newLi = document.createElement('li')
@@ -25,7 +26,9 @@ function fnRender(data) {
         .join("")}</p>
       <div class="d-flex align-items-center justify-content-between text-light">
         <button onclick="fnMoreInfo('${item.ytid}')" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">more</button>
-        <i onclick="fnAddLocData('${item.ytid}')" class="bi bi-heart"></i>
+        <i onclick="fnAddLocData('${item.ytid}')" 
+        class="${arrLocDataHeart.find((i)=>i.ytid == item.ytid) ? "bi bi-heart-fill": 
+        "bi bi-heart"}"></i>
       </div>
     </div>
   </div>`
@@ -85,16 +88,16 @@ function fnMoreInfo(id){
     `<div class="modal-content">
     <div class="modal-header">
       <h1 class="modal-title fs-5" id="exampleModalLabel">${item.Title}</h1>
-      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      <button onclick="pauseVideo()" type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
     </div>
-    <div class="modal-body ">
-      <iframe width="100%" height="400" src="https://www.youtube.com/embed/${item.ytid}" title="КОГДА НОВЫЙ ВОЗВРАТ UC?! УТЕЧКИ НОВОЙ КОЛЛАБОРАЦИИ?! ТРАНСФОРМЕРЫ В PUBG MOBILE?!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <div class="modal-body " id="youtubePlayer">
+      <iframe  width="100%" height="400" src="https://www.youtube.com/embed/${item.ytid}" title="КОГДА НОВЫЙ ВОЗВРАТ UC?! УТЕЧКИ НОВОЙ КОЛЛАБОРАЦИИ?! ТРАНСФОРМЕРЫ В PUBG MOBILE?!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       <p>${item.summary}</p>
     </div>
     <div class="modal-footer d-flex align-items-center justify-content-between">
       <span class="d-flex align-items-center">
         <p>${Math.floor(item.runtime / 60) +":" + item.runtime % 60}</p>
-        <p class="ms-3 bg-success p-2 ">${item.Categories}</p>
+        <p class="ms-3 bg-success p-2 text-light ">${item.Categories}</p>
       </span>
       <span>
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -108,27 +111,66 @@ function fnMoreInfo(id){
 let elFavMov = document.querySelector('.favourite__move')
 let locData = []
 function fnAddLocData(id){
+    if(window.localStorage.getItem('locdata')){
+    locData = JSON.parse(window.localStorage.getItem('locdata'))
+    }
     let item = partMovies.find(i=> i.ytid == id)
+    if(locData.find((i)=> i.ytid == item.ytid)){
+      window.localStorage.setItem('locdata', JSON.stringify(locData.filter((k)=> k.ytid != id)))
+    }else{
     locData.push(item)
-    console.log(locData);
     window.localStorage.setItem('locdata', JSON.stringify(locData))
+    }
+    fnRender(partMovies)
+
 }
 
 function fnRenLoc(){
+  elFavMov.innerHTML = ''
   let favouritMovie = JSON.parse(window.localStorage.getItem('locdata'))
   favouritMovie.forEach((item)=>{
     let newLI = document.createElement('li')
     newLI.innerHTML = `
-    <div class="border-1 d-flex align-items-center"> 
+    <div class="border-1 d-flex align-items-center" onclick="fnMoreInfo('${item.ytid}')" 
+    data-bs-toggle="modal" data-bs-target="#exampleModal"> 
       <img height="80" src="https://i.ytimg.com/vi/${item.ytid}/hqdefault.jpg?" alt="">
       <p class="ms-3">${item.Title.toString()
         .split("")
         .slice(0, 12)
         .join("")}</p>
-    </div>
+        </div>
     `
     elFavMov.appendChild(newLI)
   })
 }
-
 fnRenLoc()
+
+function pauseVideo(){
+    var youtubePlayer = document.getElementById('youtubePlayer');
+    setTimeout(()=>{
+      youtubePlayer.innerHTML = ''
+    },500)
+}
+
+let count = 0
+function partPagenation(part){
+  count = part
+  fnRender(partMovies.slice((part - 1) * 10,part * 10));
+}
+
+function fnPagenation(val){
+  if(val == 'prev' && count >= 2){
+    fnRender(partMovies.slice((count - 2) * 10,(count-1) * 10));
+    count = count - 1
+  }else if(val == 'next'){
+    fnRender(partMovies.slice(count * 10,(count + 1) * 10));
+    count = count + 1
+  }
+}
+
+for(let i = 1; i<= partMovies.length / 10; i++){
+    document.querySelector('.pagenation__inner').innerHTML += `
+    <li class="page-item"><button onclick="partPagenation(${i})"
+    class="page-link bg-light text-black fw-bold">${i}</button></li>
+      `
+}
